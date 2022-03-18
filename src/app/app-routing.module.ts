5088d0e7f613adced60f26b76d986a13ed969f85
pradeep <pradeep.j@metamation.com>
2022-03-18 16:12:56 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PasswordFormComponent } from './components/password-form/password-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PasswordFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PasswordFormComponent } from './components/password-form/password-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PasswordFormComponent
   ],
   imports: [
     BrowserModule,
